1c942a7d22aa0df44b3e3d0e1de13cea8cc4f494
Mateusz Lach <235910@edu.p.lodz.pl>
2021-02-22 23:59:50 +0100

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/untitled9.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled9.dir/main.cpp.obj"
+  "untitled9.exe"
+  "untitled9.exe.manifest"
+  "untitled9.lib"
+  "untitled9.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled9.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
